2c7b68b917b376d0e3fe41bdfd802c5a115f9cba
Ben Chambers <bjchambers@gmail.com>
2012-09-11 20:43:07 -0700

    Correct the project name.

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "example"
+    val appName         = "js-pomodoro"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
