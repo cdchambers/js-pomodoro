5ebcbf9a076da2384e550e28e6cf22ac9d39cb92
Christine Chambers <christine.d.hang@gmail.com>
2012-09-12 22:59:12 -0700

    Finish the initital tutorial to create a very simple tasks app.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,37 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import models.Task
 
-object Application extends Controller {
-  
+object Application       extends Controller {
+
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Redirect(routes.Application.tasks)
   }
-  
+
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(views.html.index(Task.all(), formWithErrors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
+
+  def deleteTask(id: Long) = Action {
+    Task.delete(id)
+    Redirect(routes.Application.tasks)
+  }
+
 }
