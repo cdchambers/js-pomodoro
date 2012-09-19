3d1eb61e25cefa4118c66c19958dba6708bcc52e
Christine Chambers <christine.d.hang@gmail.com>
2012-09-18 20:39:45 -0700

    Added a javascript timer and the ability to select a task.

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -1,5 +1,32 @@
-var timer = function() {
-  setTimeout(function() {
-    alert("surprise");
-  }, 500);
+$(document).ready(function() {
+  $(".done-false").click(toggleTaskSelection);
+});
+
+var refreshIntervalID;
+
+var startTimer = function() {
+  $("#startTimer").attr("disabled", true);
+  var startTime = (new Date()).getTime();
+  refreshIntervalID = window.setInterval(function() {
+    var elapsedTime = (new Date()).getTime() - startTime;
+    if(elapsedTime < 25 * 60 * 1000) {
+      var remainingTimeForDisplay = 25 - Math.round(elapsedTime / (60 * 1000));
+      $("#timeDisplay").html(remainingTimeForDisplay + " minutes remaining");
+    } else{
+      window.clearInterval(refreshIntervalID);
+      $("#timeDisplay").html("pomodoro completed");
+      $("#startTimer").attr("disabled", true);
+    }
+  }, 5000);
+};
+
+var resetTimer = function() {
+  window.clearInterval(refreshIntervalID);
+  $("#timeDisplay").html("25 minutes remaining");
+  $("#startTimer").attr("disabled", false);
+};
+
+var toggleTaskSelection = function() {
+  console.log(this);
+  $(this).toggleClass("selected");
 };
